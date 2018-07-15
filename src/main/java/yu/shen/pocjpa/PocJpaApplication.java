067b3caf16d23bd1fe23d912aa5ea4f2e4343cd4
Shen, Yu <shenyu1997@gmail.com>
2018-07-15 21:47:54 +0800

    init

---
+++ src/main/java/yu/shen/pocjpa/PocJpaApplication.java
@@ -0,0 +1,12 @@
+package yu.shen.pocjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocJpaApplication.class, args);
+	}
+}
